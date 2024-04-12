c24d7c344f78cf84d5d07aeb39dfdcbd1f57f7b2
ktunchar <maikittitee1615@gmail.com>
2024-04-12 20:53:38 +0700

    test: stack

--- rank5/calc/main.c
+++ rank5/calc/main.c
@@ -13,9 +13,12 @@ t_stack *init_stack(int size)
 {
 	t_stack *st;
 
+	st = malloc(sizeof(t_stack));
+
 	st->size = size;
 	st->items = calloc(sizeof(char), size);
 	st->sp = 0;
+	return (st);
 }
 
 void	add(t_stack *stack, char new)
@@ -27,6 +30,9 @@ void	add(t_stack *stack, char new)
 char	pop(t_stack *stack)
 {
 	char ret;
+
+	if (stack->sp == 0)
+		return (0);
 
 	stack->sp -= 1;
 	ret = stack->items[stack->sp];
@@ -60,10 +66,69 @@ int	in(t_stack *stack, char q)
 	return (0);
 }
 
+void	print_stack(t_stack *stack)
+{
+	size_t i;
+
+	i = 0;
+	while (i < stack->sp)
+	{
+		printf("%c ",stack->items[i]);
+		i++;
+	}
+	printf("\n");
+
+
+}
+
 int	main(int ac, char **av)
 {
 	// char *postfix;
+	t_stack *st;
 
+	st = init_stack(1000);
+
+	add(st, 'A');
+	print_stack(st);
+	printf("peak: %c\n", peak(st));
+	
+	add(st, 'b');
+	print_stack(st);
+	printf("peak: %c\n", peak(st));
+	
+	add(st, 'c');
+	print_stack(st);
+	printf("peak: %c\n", peak(st));
+	
+	add(st, 'd');
+	print_stack(st);
+	printf("peak: %c\n", peak(st));
+	
+	add(st, 'e');
+	print_stack(st);
+	printf("peak: %c\n", peak(st));
+
+
+	printf("pop %c\n", pop(st));
+	print_stack(st);
+	printf("peak: %c\n", peak(st));
+
+	printf("pop %c\n", pop(st));
+	print_stack(st);
+	printf("peak: %c\n", peak(st));
+	
+
+	printf("pop %c\n", pop(st));
+	print_stack(st);
+	printf("peak: %c\n", peak(st));
+	
+	printf("pop %c\n", pop(st));
+	print_stack(st);
+	printf("peak: %c\n", peak(st));
+	
+	printf("pop %c\n", pop(st));
+	print_stack(st);
+	printf("peak: %c\n", peak(st));
 	// if (ac != 2)
 	// {
 	// 	dprintf(stderr,"program take 1 argument\n");
