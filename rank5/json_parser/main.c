6bc7fe009a2059144774c8f7a975a0f77d7b87df
ktunchar <maikittitee1615@gmail.com>
2024-04-16 03:48:50 +0700

    print_json

--- rank5/json_parser/main.c
+++ rank5/json_parser/main.c
@@ -28,18 +28,58 @@ struct pair
     struct json value;
 };
 
-
-void	print_json(struct json data)
+void	nchar(char a, int n)
 {
+	int i;
 
+	i = 0;
+	while (i < n)
+	{
+		printf("%c", a);
+		i++;
+	}
+}
+
+void	print_pair(struct pair data, int indent);
+
+void	print_json(struct json data, int indent)
+{
+	nchar('\t', indent);
+	if (data.type == STRING)
+		printf("\"%s\"", data.string);
+	if (data.type == INTEGER)
+		printf("#%d", data.integer);
+	if (data.type == MAP)
+	{
+		print_pair(*data.map.data, indent + 1);
+	}
 
 }
 
-void	print_pair(struct pair data)
+void	print_pair(struct pair data, int indent)
 {
+	nchar('\t', indent);
 	// key
-	printf("key: %s\n", data.key);
+	printf("%s:", data.key);
 
 	// json value
-	print_json(data.value);
+	print_json(data.value, indent);
+}
+
+int	main(void)
+{
+	struct pair p;
+
+	struct pair p2;
+
+	p2.key = "inside";
+	p2.value.type = STRING;
+	p2.value.string = "yed mae";
+
+	p.key = "hello";
+	p.value.type = MAP;
+	p.value.map.data = &p2;
+
+
+	print_pair(p, 0);
 }
